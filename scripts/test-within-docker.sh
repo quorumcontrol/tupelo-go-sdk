be4fd5942d6d44f0894d32e1ef6142875f40b6cf
Arve Knudsen <arve.knudsen@gmail.com>
2019-04-23 15:27:45 +0200

    CircleCI: Download deps before linting

--- scripts/test-within-docker.sh
+++ scripts/test-within-docker.sh
@@ -5,6 +5,7 @@ mkdir -p ~/.ssh
 git config --global url."ssh://git@github.com/".insteadOf "https://github.com/"
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 
+go mod download
 make lint
 
 if [[ "${CI}" == "true" ]]; then
