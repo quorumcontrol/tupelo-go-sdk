b0c55276a0014287baac067fc65679604782439a
Topper Bowers <topper@toppingdesign.com>
2019-07-08 12:25:50 +0200

    pubsub works

--- p2p/standalone/main.go
+++ p2p/standalone/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/pkg/errors"
@@ -11,10 +12,10 @@ import (
 
 // Standalone server for testing libp2p-js / libp2p-go interop
 
-func startServer(ctx context.Context) error {
+func startServer(ctx context.Context) (p2p.Node, error) {
 	key, err := crypto.GenerateKey()
 	if err != nil {
-		return errors.Wrap(err, "error generating key")
+		return nil, errors.Wrap(err, "error generating key")
 	}
 
 	opts := []p2p.Option{
@@ -22,26 +23,29 @@ func startServer(ctx context.Context) error {
 		p2p.WithWebSockets(true),
 	}
 
-	host, err := p2p.NewHostFromOptions(ctx, opts...)
-	if err != nil {
-		return errors.Wrap(err, "error creating host")
-	}
-
-	fmt.Println("standalone node running at:")
-	for _, addr := range host.Addresses() {
-		fmt.Println(addr)
-	}
-
-	return nil
+	return p2p.NewHostFromOptions(ctx, opts...)
 }
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	err := startServer(ctx)
+	h, err := startServer(ctx)
 	if err != nil {
 		panic(err)
 	}
-	select {}
+	fmt.Println("standalone node running at:")
+	for _, addr := range h.Addresses() {
+		fmt.Println(addr)
+	}
+
+	ps := h.GetPubSub()
+	i := 0
+	for {
+		msg := fmt.Sprintf("hi - %d", i)
+		ps.Publish("test", []byte(msg))
+		fmt.Println(msg)
+		i++
+		time.Sleep(2 * time.Second)
+	}
 }
