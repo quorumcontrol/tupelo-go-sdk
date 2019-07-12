55370a9f85aa83b1312e74cfacb31828f604f515
Topper Bowers <topper@toppingdesign.com>
2019-07-12 11:51:03 +0200

    remove dead code

--- wasm/main.go
+++ wasm/main.go
@@ -8,11 +8,6 @@ import (
 	"fmt"
 	"syscall/js"
 
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/quorumcontrol/chaintree/chaintree"
-	"github.com/quorumcontrol/chaintree/nodestore"
-	"github.com/quorumcontrol/messages/build/go/transactions"
-	"github.com/quorumcontrol/tupelo-go-sdk/consensus"
 	"github.com/quorumcontrol/tupelo-go-sdk/wasm/pubsub"
 	"github.com/quorumcontrol/tupelo-go-sdk/wasm/then"
 	"github.com/quorumcontrol/tupelo-go-sdk/wasm/jsclient"
@@ -24,52 +19,33 @@ func init() {
 	exitChan = make(chan bool)
 }
 
-func doIt() error {
-	ctx := context.TODO()
-
-	key, err := crypto.GenerateKey()
-	if err != nil {
-		return err
-	}
-
-	store := nodestore.MustMemoryStore(ctx)
-	treeDID := consensus.AddrToDid(crypto.PubkeyToAddress(key.PublicKey).String())
-	emptyTree := consensus.NewEmptyTree(treeDID, store)
-
-	tokenName := "testtoken"
-	// tokenFullName := strings.Join([]string{treeDID, tokenName}, ":")
-
-	txn, err := chaintree.NewEstablishTokenTransaction(tokenName, 42)
-	if err != nil {
-		return err
-	}
-
-	blockWithHeaders := chaintree.BlockWithHeaders{
-		Block: chaintree.Block{
-			PreviousTip:  nil,
-			Height:       0,
-			Transactions: []*transactions.Transaction{txn},
-		},
-	}
-
-	testTree, err := chaintree.NewChainTree(ctx, emptyTree, nil, consensus.DefaultTransactors)
-	if err != nil {
-		return err
-	}
-
-	_, err = testTree.ProcessBlock(ctx, &blockWithHeaders)
-	if err != nil {
-		return err
-	}
-	return nil
+func testPubSub(jsPubSubLibrary js.Value) *then.Then {
+	t := then.New()
+	go func() {
+		gopub := pubsub.NewPubSubBridge(jsPubSubLibrary)
+		sub,err := gopub.Subscribe("test")
+		if err != nil {
+			t.Reject(err.Error())
+		} 
+		ctx,cancel := context.WithTimeout(context.Background(), 2 * time.Second)
+		defer cancel()
+		
+		err = gopub.Publish("test", []byte("hi"))
+		if err != nil {
+			t.Reject(err.Error())
+		} 
+		
+		msg,err := sub.Next(ctx)
+		if err == nil {
+			t.Resolve(js.TypedArrayOf(msg.Data))
+		} else {
+			t.Reject(err.Error())
+		}
+	}()
+	return t
 }
 
 func main() {
-	// err := doIt()
-	// if err != nil {
-	// 	panic(err)
-	// }
-
 	js.Global().Get("Go").Set("exit", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		exitChan <- true
 		return nil
@@ -85,34 +61,11 @@ func main() {
 			jsObj := args[0]
 
 			jsObj.Set("generateKey", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-				c := jsclient.New()
-				return c.GenerateKey()
+				return jsclient.New().GenerateKey()
 			}))
 
 			jsObj.Set("testpubsub", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-				t := then.New()
-				go func() {
-					gopub := pubsub.NewPubSubBridge(args[0])
-					sub,err := gopub.Subscribe("test")
-					if err != nil {
-						t.Reject(err.Error())
-					} 
-					ctx,cancel := context.WithTimeout(context.Background(), 2 * time.Second)
-					defer cancel()
-					
-					err = gopub.Publish("test", []byte("hi"))
-					if err != nil {
-						t.Reject(err.Error())
-					} 
-					
-					msg,err := sub.Next(ctx)
-					if err == nil {
-						t.Resolve(js.TypedArrayOf(msg.Data))
-					} else {
-						t.Reject(err.Error())
-					}
-				}()
-				return t
+				return testPubSub(args[0])
 			}))
 			return jsObj
 		}),
