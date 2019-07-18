f65b321e73b5f8c3ff1c9e95f59bf4eac5d13b9a
Topper Bowers <topper@toppingdesign.com>
2019-07-18 12:24:58 +0200

    remove bad flag

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -31,10 +31,6 @@ import (
 	"github.com/quorumcontrol/tupelo-go-sdk/gossip3/middleware"
 )
 
-func init() {
-	peer.AdvancedEnableInlining = false
-}
-
 var log = logging.Logger("tupelop2p")
 
 var ErrDialBackoff = swarm.ErrDialBackoff
