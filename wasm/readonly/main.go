0720e3d4ff1d1646257e50e56452bc8536ee06cc
Topper Bowers <topper@toppingdesign.com>
2019-08-16 11:19:24 +0200

    add a readonly package

---
+++ wasm/readonly/main.go
@@ -0,0 +1,58 @@
+// +build wasm
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"syscall/js"
+
+	"github.com/quorumcontrol/tupelo-go-sdk/wasm/jscommunity"
+	"github.com/quorumcontrol/tupelo-go-sdk/wasm/jslibs"
+)
+
+var exitChan chan bool
+
+func init() {
+	exitChan = make(chan bool)
+}
+
+func main() {
+	js.Global().Get("Go").Set("exit", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		exitChan <- true
+		return nil
+	}))
+
+	js.Global().Set(
+		"populateLibrary",
+		js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			if len(args) != 2 || !args[0].Truthy() || !args[1].Truthy() {
+				err := fmt.Errorf("error, must supply a valid object")
+				panic(err)
+			}
+
+			helperLibs := args[1]
+			cids := helperLibs.Get("cids")
+			ipfsBlock := helperLibs.Get("ipfs-block")
+			if !cids.Truthy() || !ipfsBlock.Truthy() {
+				err := fmt.Errorf("error, must supply a library object containing cids and ipfs-block")
+				go fmt.Println(err.Error())
+				panic(err)
+			}
+			jslibs.Cids = cids
+			jslibs.IpfsBlock = ipfsBlock
+
+			jsObj := args[0]
+
+			jsObj.Set("getCurrentState", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+				jsOpts := args[0]
+				return jscommunity.GetCurrentState(context.TODO(), jsOpts.Get("tip"), jsOpts.Get("blockService"), jsOpts.Get("did"))
+			}))
+			return jsObj
+		}),
+	)
+
+	go js.Global().Get("Go").Call("readyResolver")
+
+	<-exitChan
+}
