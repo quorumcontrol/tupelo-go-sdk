914e453b72667d726e8d56514089dd7a39c96bdd
Topper Bowers <topper@toppingdesign.com>
2019-09-04 14:22:17 +0200

    passing tests using new protobufs

--- signatures/signature_test.go
+++ signatures/signature_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/quorumcontrol/messages/build/go/signatures"
 	"github.com/quorumcontrol/tupelo-go-sdk/bls"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -15,24 +16,24 @@ func TestEcdsaAddress(t *testing.T) {
 	require.Nil(t, err)
 
 	// With no conditions it's the same as a normal key
-	o := &Ownership{
-		Type:      KeyTypeSecp256k1,
+	o := &signatures.Ownership{
+		Type:      signatures.Ownership_KeyTypeSecp256k1,
 		PublicKey: crypto.FromECDSAPub(&key.PublicKey),
 	}
-	addr, err := o.Address()
+	addr, err := Address(o)
 	require.Nil(t, err)
 	assert.Equal(t, addr.String(), crypto.PubkeyToAddress(key.PublicKey).String())
 
 	// with conditions, it changes the addr
 	o.Conditions = "true"
-	conditionalAddr, err := o.Address()
+	conditionalAddr, err := Address(o)
 	require.Nil(t, err)
 	assert.NotEqual(t, conditionalAddr.String(), crypto.PubkeyToAddress(key.PublicKey).String())
 	assert.Len(t, conditionalAddr, 20) // same length as an addr
 
 	// changing the conditions changes the addr
 	o.Conditions = "false"
-	conditionalAddr2, err := o.Address()
+	conditionalAddr2, err := Address(o)
 	require.Nil(t, err)
 	assert.NotEqual(t, conditionalAddr2.String(), conditionalAddr.String())
 	assert.Len(t, conditionalAddr2, 20)
@@ -42,24 +43,24 @@ func TestBLSAddr(t *testing.T) {
 	key := bls.MustNewSignKey()
 
 	// With no conditions it's the same as a normal key
-	o := &Ownership{
-		Type:      KeyTypeBLSGroupSig,
+	o := &signatures.Ownership{
+		Type:      signatures.Ownership_KeyTypeBLSGroupSig,
 		PublicKey: key.MustVerKey().Bytes(),
 	}
-	addr, err := o.Address()
+	addr, err := Address(o)
 	require.Nil(t, err)
 	assert.Equal(t, addr.String(), bytesToAddress(o.PublicKey).String())
 
 	// with conditions, it changes the addr
 	o.Conditions = "true"
-	conditionalAddr, err := o.Address()
+	conditionalAddr, err := Address(o)
 	require.Nil(t, err)
 	assert.NotEqual(t, conditionalAddr.String(), bytesToAddress(o.PublicKey).String())
 	assert.Len(t, conditionalAddr, 20) // same length as an addr
 
 	// changing the conditions changes the addr
 	o.Conditions = "false"
-	conditionalAddr2, err := o.Address()
+	conditionalAddr2, err := Address(o)
 	require.Nil(t, err)
 	assert.NotEqual(t, conditionalAddr2.String(), conditionalAddr.String())
 	assert.Len(t, conditionalAddr2, 20)
@@ -72,16 +73,16 @@ func TestEcdsaKeyRestore(t *testing.T) {
 
 	sigBits, err := crypto.Sign(msg, key)
 	require.Nil(t, err)
-	sig := &Signature{
-		Ownership: &Ownership{
-			Type: KeyTypeSecp256k1,
+	sig := &signatures.Signature{
+		Ownership: &signatures.Ownership{
+			Type: signatures.Ownership_KeyTypeSecp256k1,
 		},
 		Signature: sigBits,
 	}
-	err = sig.RestorePublicKey(msg)
+	err = RestorePublicKey(sig, msg)
 	require.Nil(t, err)
-	assert.Len(t, sig.PublicKey, 65)
-	assert.Equal(t, crypto.FromECDSAPub(&key.PublicKey), sig.PublicKey)
+	assert.Len(t, sig.Ownership.PublicKey, 65)
+	assert.Equal(t, crypto.FromECDSAPub(&key.PublicKey), sig.Ownership.PublicKey)
 }
 
 func TestEcdsaSigning(t *testing.T) {
@@ -91,14 +92,14 @@ func TestEcdsaSigning(t *testing.T) {
 
 	sigBits, err := crypto.Sign(msg, key)
 	require.Nil(t, err)
-	sig := &Signature{
-		Ownership: &Ownership{
-			Type: KeyTypeSecp256k1,
+	sig := &signatures.Signature{
+		Ownership: &signatures.Ownership{
+			Type: signatures.Ownership_KeyTypeSecp256k1,
 		},
 		Signature: sigBits,
 	}
-	sig.RestorePublicKey(msg)
-	verified, err := sig.Valid(msg, nil)
+	RestorePublicKey(sig, msg)
+	verified, err := Valid(sig, msg, nil)
 	require.Nil(t, err)
 	assert.True(t, verified)
 }
@@ -110,21 +111,21 @@ func TestEcdsaSigningWithConditions(t *testing.T) {
 
 	sigBits, err := crypto.Sign(msg, key)
 	require.Nil(t, err)
-	sig := &Signature{
-		Ownership: &Ownership{
-			Type:       KeyTypeSecp256k1,
+	sig := &signatures.Signature{
+		Ownership: &signatures.Ownership{
+			Type:       signatures.Ownership_KeyTypeSecp256k1,
 			Conditions: "false",
 		},
 		Signature: sigBits,
 	}
-	sig.RestorePublicKey(msg)
-	verified, err := sig.Valid(msg, nil)
+	RestorePublicKey(sig, msg)
+	verified, err := Valid(sig, msg, nil)
 	require.Nil(t, err)
 	// Conditions returned false so it should not verify
 	assert.False(t, verified)
 
-	sig.Conditions = "true"
-	verified, err = sig.Valid(msg, nil)
+	sig.Ownership.Conditions = "true"
+	verified, err = Valid(sig, msg, nil)
 	require.Nil(t, err)
 	// Conditions are now TRUE so should verify
 	assert.True(t, verified)
@@ -140,22 +141,22 @@ func TestHashPreimageConditions(t *testing.T) {
 
 	sigBits, err := crypto.Sign(msg, key)
 	require.Nil(t, err)
-	sig := &Signature{
-		Ownership: &Ownership{
-			Type:       KeyTypeSecp256k1,
+	sig := &signatures.Signature{
+		Ownership: &signatures.Ownership{
+			Type:       signatures.Ownership_KeyTypeSecp256k1,
 			Conditions: fmt.Sprintf(`(== (hashed-preimage) "%s")`, hsh),
 		},
 		Signature: sigBits,
 		PreImage:  "not the right one",
 	}
-	sig.RestorePublicKey(msg)
-	verified, err := sig.Valid(msg, nil)
+	RestorePublicKey(sig, msg)
+	verified, err := Valid(sig, msg, nil)
 	require.Nil(t, err)
 	// Conditions returned false so it should not verify
 	assert.False(t, verified)
 
 	sig.PreImage = preImage
-	verified, err = sig.Valid(msg, nil)
+	verified, err = Valid(sig, msg, nil)
 	require.Nil(t, err)
 	// Conditions are now TRUE so should verify
 	assert.True(t, verified)
@@ -171,20 +172,20 @@ func BenchmarkWithConditions(b *testing.B) {
 
 	sigBits, err := crypto.Sign(msg, key)
 	require.Nil(b, err)
-	sig := &Signature{
-		Ownership: &Ownership{
-			Type:       KeyTypeSecp256k1,
+	sig := &signatures.Signature{
+		Ownership: &signatures.Ownership{
+			Type:       signatures.Ownership_KeyTypeSecp256k1,
 			Conditions: fmt.Sprintf(`(== (hashed-preimage) "%s")`, hsh),
 		},
 		Signature: sigBits,
 		PreImage:  preImage,
 	}
-	sig.RestorePublicKey(msg)
+	RestorePublicKey(sig, msg)
 
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		_, err = sig.Valid(msg, nil)
+		_, err = Valid(sig, msg, nil)
 	}
 	require.Nil(b, err)
 }
@@ -196,19 +197,19 @@ func BenchmarkWithoutConditions(b *testing.B) {
 
 	sigBits, err := crypto.Sign(msg, key)
 	require.Nil(b, err)
-	sig := &Signature{
-		Ownership: &Ownership{
-			Type:       KeyTypeSecp256k1,
+	sig := &signatures.Signature{
+		Ownership: &signatures.Ownership{
+			Type:       signatures.Ownership_KeyTypeSecp256k1,
 			Conditions: "",
 		},
 		Signature: sigBits,
 	}
-	sig.RestorePublicKey(msg)
+	RestorePublicKey(sig, msg)
 
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		_, err = sig.Valid(msg, nil)
+		_, err = Valid(sig, msg, nil)
 	}
 	require.Nil(b, err)
 }
