f5665511b421627947173265f27c72e8317476bd
Wes Morgan <wesmorgan@icloud.com>
2019-10-03 11:14:26 -0600

    Use ~/.ssh dir for CI scripts

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -2,10 +2,10 @@
 set -eo pipefail
 
 mkdir -p ~/.ssh
-echo "$SSH_PRIVATE_KEY" > ~/ssh/id_rsa
-chmod 600 ~/ssh/id_rsa
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
 eval "$(ssh-agent -s)" > /dev/null 2>&1
-ssh-add ~/ssh/id_rsa > /dev/null 2>&1
+ssh-add ~/.ssh/id_rsa > /dev/null 2>&1
 
 go mod download
 make lint
