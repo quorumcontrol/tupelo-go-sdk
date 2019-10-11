cc7925f068e635ed32e1c3eae9cb9d33e498cfb9
Topper Bowers <topper@toppingdesign.com>
2019-10-11 11:23:03 +0200

    add a CurrentState verification

---
+++ client/verification.go
@@ -0,0 +1,43 @@
+package client
+
+import (
+	"context"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/quorumcontrol/messages/build/go/signatures"
+	"github.com/quorumcontrol/tupelo-go-sdk/bls"
+	"github.com/quorumcontrol/tupelo-go-sdk/consensus"
+	"github.com/quorumcontrol/tupelo-go-sdk/gossip3/types"
+	"golang.org/x/xerrors"
+)
+
+func VerifyCurrentState(ctx context.Context, group *types.NotaryGroup, state *signatures.CurrentState) (bool, error) {
+	sp, ctx := opentracing.StartSpanFromContext(ctx, "verifyCurrentState")
+	defer sp.Finish()
+
+	var verKeys [][]byte
+
+	signers := group.AllSigners()
+	var signerCount uint64
+	for i, cnt := range state.Signature.Signers {
+		if cnt > 0 {
+			signerCount++
+			verKey := signers[i].VerKey.Bytes()
+			newKeys := make([][]byte, cnt)
+			for j := uint32(0); j < cnt; j++ {
+				newKeys[j] = verKey
+			}
+			verKeys = append(verKeys, newKeys...)
+		}
+	}
+	if signerCount < group.QuorumCount() {
+		return false, nil
+	}
+	isVerified, err := bls.VerifyMultiSig(state.Signature.Signature, consensus.GetSignable(state.Signature), verKeys)
+	if err != nil {
+		sp.SetTag("error", true)
+		return false, xerrors.Errorf("error verifying: %w", err)
+	}
+	sp.SetTag("verified", isVerified)
+	return true, nil
+}
