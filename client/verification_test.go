3e39fbcf6ff9b945e0f7a8198ee915ee2ef85ed1
Topper Bowers <topper@toppingdesign.com>
2019-10-27 08:24:47 -0700

    update verification to latest

--- client/verification_test.go
+++ client/verification_test.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"testing"
 
+	sigfuncs "github.com/quorumcontrol/tupelo-go-sdk/signatures"
+
 	"github.com/quorumcontrol/chaintree/safewrap"
 	"github.com/quorumcontrol/messages/build/go/signatures"
-	"github.com/quorumcontrol/tupelo-go-sdk/bls"
 	"github.com/quorumcontrol/tupelo-go-sdk/consensus"
 	"github.com/quorumcontrol/tupelo-go-sdk/gossip3/types"
 	"github.com/quorumcontrol/tupelo-go-sdk/testnotarygroup"
@@ -20,43 +21,39 @@ func TestVerifyCurrentState(t *testing.T) {
 
 	ts := testnotarygroup.NewTestSet(t, 3)
 	ng := types.NewNotaryGroup("testverification")
-	signer1 := types.NewLocalSigner(consensus.PublicKeyToEcdsaPub(&ts.PubKeys[0]), ts.SignKeys[0])
-	signer2 := types.NewLocalSigner(consensus.PublicKeyToEcdsaPub(&ts.PubKeys[1]), ts.SignKeys[1])
-	signer3 := types.NewLocalSigner(consensus.PublicKeyToEcdsaPub(&ts.PubKeys[2]), ts.SignKeys[2])
+	signer1 := types.NewLocalSigner(ts.PubKeys[0], ts.SignKeys[0])
+	signer2 := types.NewLocalSigner(ts.PubKeys[1], ts.SignKeys[1])
+	signer3 := types.NewLocalSigner(ts.PubKeys[2], ts.SignKeys[2])
 
 	ng.AddSigner(signer1)
 	ng.AddSigner(signer2)
 	ng.AddSigner(signer3)
 
 	sw := &safewrap.SafeWrap{}
-	currState := &signatures.CurrentState{
-		Signature: &signatures.Signature{
-			ObjectId: []byte("did:tupelo:fake"),
-			NewTip:   sw.WrapObject(true).Cid().Bytes(),
-		},
+	currState := &signatures.TreeState{
+		ObjectId:  []byte("did:tupelo:fake"),
+		NewTip:    sw.WrapObject(true).Cid().Bytes(),
+		Signature: &signatures.Signature{},
 	}
-	signable := consensus.GetSignable(currState.Signature)
+	signable := consensus.GetSignable(currState)
 
 	isValid, err := VerifyCurrentState(ctx, ng, currState)
 	require.Nil(t, err)
 	assert.False(t, isValid)
 
 	// now go ahead and sign it
-	signers := []uint32{1, 1, 1}
-	currState.Signature.Signers = signers
-
-	sig1, err := ts.SignKeys[0].Sign(signable)
+	sig1, err := sigfuncs.BLSSign(ts.SignKeys[0], signable, 3, 0)
 	require.Nil(t, err)
 
-	sig2, err := ts.SignKeys[1].Sign(signable)
+	sig2, err := sigfuncs.BLSSign(ts.SignKeys[1], signable, 3, 1)
 	require.Nil(t, err)
 
-	sig3, err := ts.SignKeys[2].Sign(signable)
-	require.Nil(t, err)
-	sig, err := bls.SumSignatures([][]byte{sig1, sig2, sig3})
+	sig3, err := sigfuncs.BLSSign(ts.SignKeys[2], signable, 3, 2)
 	require.Nil(t, err)
 
-	currState.Signature.Signature = sig
+	sig, err := sigfuncs.AggregateBLSSignatures([]*signatures.Signature{sig1, sig2, sig3})
+
+	currState.Signature = sig
 
 	isValid, err = VerifyCurrentState(ctx, ng, currState)
 	require.Nil(t, err)
