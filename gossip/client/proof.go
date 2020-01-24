7184c1952ee0ebde4e95e5825913558af0d49752
ben lamothe <bl@732.one>
2020-01-24 04:22:14 -0500

    don't register proof types with cbor (happens in chaintree)

--- gossip/client/proof.go
+++ gossip/client/proof.go
@@ -2,14 +2,8 @@ package client
 
 import (
 	"github.com/ipfs/go-cid"
-	cbornode "github.com/ipfs/go-ipld-cbor"
-	"github.com/quorumcontrol/messages/v2/build/go/gossip"
 	"github.com/quorumcontrol/tupelo-go-sdk/gossip/types"
 )
-
-func init() {
-	cbornode.RegisterCborType(gossip.Proof{})
-}
 
 type ValidationNotification struct {
 	AbrCid            cid.Cid
